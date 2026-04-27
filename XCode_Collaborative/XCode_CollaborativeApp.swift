a6e0a29d6b308b1c184fcd1422ebe2e1e614cd66
BT_C09_034 <mobileappdev@bths.edu>
2026-04-27 09:26:53 -0400

    Initial Commit

---
+++ XCode_Collaborative/XCode_CollaborativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCode_CollaborativeApp.swift
+//  XCode_Collaborative
+//
+//  Created by Student on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct XCode_CollaborativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
