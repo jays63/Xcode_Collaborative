9c9991335d85ec64d578b6bfd4f3b4a106f3a5ea
BT_C09_034 <mobileappdev@bths.edu>
2026-04-29 09:31:01 -0400

    Well have we even started?

--- XCode_Collaborative/ContentView.swift
+++ XCode_Collaborative/ContentView.swift
@@ -3,7 +3,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            
+            Text("")
+                .multilineTextAlignment(.center)
         }
         .padding()
     }
