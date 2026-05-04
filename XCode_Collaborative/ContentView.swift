1a7ecc95840cdf9ddc865c85cd3a4f0f7eaa7a27
BT_C09_034 <mobileappdev@bths.edu>
2026-05-04 13:47:29 -0400

    A bit more progress, still delaying making sure any of the features work and working on the API properly.

--- XCode_Collaborative/ContentView.swift
+++ XCode_Collaborative/ContentView.swift
@@ -2,16 +2,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Welcome to the pocket pokedex! What do you want to do")
-                .multilineTextAlignment(.center)
-            Button("Browse dex"){}
-            Button("Visit the damage calculator"){}
-            Button("Visit the unaffiliated battle simulator"){}
-            Button("View items"){}
-            Button("Get a meme, feature may be axed"){}
+        ZStack{
+            Image(.pokeball)
+            VStack(alignment: .center, spacing: 30){
+                Text("Welcome to the pokedex! What do you want to find?")
+                    .multilineTextAlignment(.center)
+                Button("Browse dex"){}
+                Button("Find the damage calculator"){}
+                label: do {
+                    Link("Here", destination: URL(string: "https://calc.pokemonshowdown.com/")!)
+                }
+                Button("Visit the unaffiliated battle simulator"){}
+                label: do {
+                    Link("Visit the unaffiliated battle simulator", destination: URL(string: "https://pokemonshowdown.com/")!)
+                }
+                Button("View items"){}
+                Button("Get a meme, feature may be axed"){}
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
