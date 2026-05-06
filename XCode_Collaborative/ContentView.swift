0480773115444b74a762b16cc0a65dbc1a371f90
BT_C09_034 <mobileappdev@bths.edu>
2026-05-06 09:28:58 -0400

    More progress? Slightly

--- XCode_Collaborative/ContentView.swift
+++ XCode_Collaborative/ContentView.swift
@@ -1,9 +1,15 @@
 import SwiftUI
 
+enum searchCase{
+    case pokemon, item, move, ability
+}
+
 struct ContentView: View {
     @Environment(NetworkClient.self) private var networkClient;
     @State private var pokemonSearch: Bool = false;
-    @State private var itemSearch: Bool = false
+    @State private var itemSearch: Bool = false;
+    @State private var moveSearch: Bool = false;
+    @State private var abilitySearch: Bool = false;
     var body: some View {
         ZStack{
             Image(.pokeball)
@@ -17,6 +23,24 @@ struct ContentView: View {
                         .navigationDestination(isPresented: $pokemonSearch){
                             
                         }
+                        Button("View items"){
+                            itemSearch=true
+                        }
+                        .navigationDestination(isPresented: $itemSearch){
+                            
+                        }
+                        Button("Browse moves"){
+                            moveSearch=true
+                        }
+                        .navigationDestination(isPresented: $moveSearch){
+                            
+                        }
+                        Button("Browse abilities"){
+                            abilitySearch=true
+                        }
+                        .navigationDestination(isPresented: $abilitySearch){
+                            
+                        }
                     }
                     Button("Find the damage calculator"){}
                     label: do {
@@ -24,12 +48,18 @@ struct ContentView: View {
                     }
                     Button("Visit the unaffiliated battle simulator"){}
                     label: do {
-                        Link("Visit the unaffiliated battle simulator", destination: URL(string: "https://pokemonshowdown.com/")!)
+                        Link("Visit the battle simulator (This app is not affiliated with the above)", destination: URL(string: "https://pokemonshowdown.com/")!)
                     }
-                    Button("View items"){}
+                    
                 }
                 .padding()
         }
+    }
+}
+
+func search(in: searchCase) async{
+    Task{
+        
     }
 }
 
