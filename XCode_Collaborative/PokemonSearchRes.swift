7ce0c148994a4dec207851f2fe3cc7ac9f9bc0bb
BT_C09_034 <mobileappdev@bths.edu>
2026-05-07 09:30:53 -0400

    More work

--- XCode_Collaborative/PokemonSearchRes.swift
+++ XCode_Collaborative/PokemonSearchRes.swift
@@ -2,7 +2,18 @@ import SwiftUI
 
 struct PokemonSearchRes :View {
     @Environment(NetworkClient.self) private var networkClient
+    @State private var selectedMon: pokemon?
+    private let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     var body: some View {
-        
+        ZStack{
+            Color(.white)
+                .ignoresSafeArea()
+            ScrollView(.vertical){
+                
+            }
+        }
     }
 }
