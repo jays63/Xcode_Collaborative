b9989af643a1f78cc9cef602bdc440d1fb589db5
BT_C09_034 <mobileappdev@bths.edu>
2026-05-28 13:54:21 -0400

    Animation

--- XCode_Collaborative/MoveSearchRes.swift
+++ XCode_Collaborative/MoveSearchRes.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct MoveSearchRes :View {
     @Environment(NetworkClient.self) private var networkClient
+    @State private var visible: Bool = true
     private let columns = [
         GridItem(.flexible()),
         GridItem(.flexible())
@@ -10,15 +11,22 @@ struct MoveSearchRes :View {
         ZStack{
             Color(.white)
                 .ignoresSafeArea()
-            VStack {
-                Text("Name: \(networkClient.annoyance.name)")
-                Text("Accuracy: \(networkClient.annoyance.accuracy)")
-                Text("Effect chance: \(networkClient.annoyance.effect_chance)")
-                Text("Damage type: \(networkClient.annoyance.damage_class.name)")
-                Text("Base power: \(networkClient.annoyance.power)")
-                Text("Uses: \(networkClient.annoyance.pp)")
-                Text("Priority: \(networkClient.annoyance.priority)")
-                Text("Effect: \(networkClient.annoyance.effect_entries.first?.short_effect)")
+            if visible{
+                VStack {
+                    Text("Name: \(networkClient.annoyance.name)")
+                    Text("Accuracy: \(networkClient.annoyance.accuracy)")
+                    Text("Effect chance: \(networkClient.annoyance.effect_chance)")
+                    Text("Damage type: \(networkClient.annoyance.damage_class.name)")
+                    Text("Base power: \(networkClient.annoyance.power)")
+                    Text("Uses: \(networkClient.annoyance.pp)")
+                    Text("Priority: \(networkClient.annoyance.priority)")
+                    Text("Effect: \(networkClient.annoyance.effect_entries.first?.short_effect)")
+                }
+                .onTapGesture {
+                    withAnimation(.linear(duration: 0.2)){
+                        visible.toggle()
+                    }
+                }
             }
         }
     }
